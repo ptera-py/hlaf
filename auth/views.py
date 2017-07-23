9eda34ca4186839660ec2b80f7160744f76b23c8
ptera-py <kurzy@yandex.ru>
2017-07-23 08:43:14 +0300

    Remember Me

--- auth/views.py
+++ auth/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views import generic
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
@@ -12,6 +12,10 @@ class LoginFormView(generic.edit.FormView):
 
     def form_valid(self, form):
         self.user = form.get_user()
+        if(self.request.POST.getlist('remember')):#Запомнить или не запомнить
+            self.request.session.set_expiry(999999999)
+        else:
+            self.request.session.set_expiry(0)
         login(self.request, self.user)
         return super(LoginFormView, self).form_valid(form)
 
