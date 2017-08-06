0facb82bc449aca899ec49af222e6f14d958077c
ptera-py <kurzy@yandex.ru>
2017-08-06 13:01:09 +0300

    Graphics1

--- metrics/views.py
+++ metrics/views.py
@@ -86,7 +86,7 @@ def f_indicate_edit(request):
         list_hum_me.append(classes_for_templ.OneMetric(flowmeter, flowmeter.indication_set,None))
     url_fordel = 'metrics:indicate_delete'
     url_foradd = 'metrics:indicate_insert'
-    caption = ['Показания', 0]
+    caption = ['Показания', 1]
     date_format = 's'
     context = {'user_obj': request.user, 'list_hum_me': list_hum_me, 'url_fordel': url_fordel, 'url_foradd': url_foradd,
                'caption': caption, 'date_format':date_format}
@@ -109,7 +109,7 @@ def f_communal_edit(request):
     url_fordel = 'metrics:communal_delete'
     url_foradd = 'metrics:communal_insert'
     date_format = 's'
-    caption = ['Тарифы', 0]
+    caption = ['Тарифы', 1]
     context = {'user_obj': request.user, 'list_hum_me': list_hum_me, 'url_fordel': url_fordel, 'url_foradd': url_foradd,
                'caption': caption, 'date_format':date_format}
     return render(request, 'metrics/metric_redactor.html', context)
